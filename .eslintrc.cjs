34ff959b7aa8e3e5e9b3f9ccb5aa05ee431fef08
Shaurya <j.shaurya@iitg.ac.in>
2025-08-13 23:13:25 +0530

    Add ESLint and Prettier configuration, update dependencies, and modify Vite server settings

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  env: { browser: true, es2021: true },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  parserOptions: { ecmaVersion: 2021, sourceType: "module" },
+  plugins: ["react"],
+  rules: {},
+  settings: {
+    react: { version: "detect" }
+  }
+};
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+        port: 3000, // Unique port for the first app
+        host: 'localhost', // Or '127.0.0.1'
+      },
 })
