30eb113f050fb86b9d2ff19c4a07ea8893294ee4
Shaurya <j.shaurya@iitg.ac.in>
2025-09-09 18:23:39 +0530

    added reducers in store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/user/userSlice';
+import containersReducer from '../features/containers/containersSlice';
+import dealersReducer from '../features/dealers/dealersSlice';
+import bookingsReducer from "../features/bookings/bookingsSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    containers: containersReducer,
+    dealers: dealersReducer,
+    bookings: bookingsReducer
   },
 });
