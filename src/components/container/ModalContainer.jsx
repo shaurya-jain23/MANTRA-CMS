1e102f7dd50bbc3c59ccd67cffebb24e34c35509
Shaurya <j.shaurya@iitg.ac.in>
2025-09-29 12:00:10 +0530

    fixed overflow scrolling of modals

--- src/components/container/ModalContainer.jsx
+++ src/components/container/ModalContainer.jsx
@@ -18,8 +18,8 @@ const useScrollLock = (isModalOpen) => {
 function ModalContainer({children, isOpen, className = ''}) {
   useScrollLock(isOpen);
   return (
-    <div className={`fixed inset-0 bg-stone-800/40 flex justify-center overflow-y-auto items-center z-50 transition-opacity m-0 duration-300 ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}>
-        <div className={`bg-white p-8 rounded-md shadow-xl w-full max-h-[90vh] transform ${className} transition-all duration-500 ease-out origin-center overflow-auto ${isOpen ? 'scale-100' : 'scale-80'}` }>
+    <div className={`fixed inset-0 bg-stone-800/40 flex justify-center overflow-y-auto md:overflow-y-scroll items-center z-50 transition-opacity m-0 duration-300 ${isOpen ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}>
+        <div className={`bg-white p-8 rounded-md shadow-xl w-full max-h-[90vh] transform ${className} transition-all duration-500 ease-out origin-center md:overflow-scroll overflow-auto ${isOpen ? 'scale-100' : 'scale-80'}` }>
             {children}
         </div>
     </div>
