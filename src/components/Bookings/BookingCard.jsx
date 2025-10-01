bbbfb2ff4af9cf40ee87e506e45a7ca3f3f8a702
Shaurya <j.shaurya@iitg.ac.in>
2025-10-01 14:09:30 +0530

    Fixed and enhanced BookingCard component and BookingsPage with payment status handling and improved UI layout

--- src/components/Bookings/BookingCard.jsx
+++ src/components/Bookings/BookingCard.jsx
@@ -1,104 +1,148 @@
 import React from 'react';
-import { CheckCircle, XCircle, SquarePen, Trash } from 'lucide-react';
+import { CheckCircle, XCircle, SquarePen, Trash, Banknote } from 'lucide-react';
 import { Button, Select } from '../index';
+import { format } from 'date-fns';
+import {calculatePaymentStatus, containerStatusMap, getPaymentStatusColor} from '../../assets/utils';
+import { useNavigate } from 'react-router-dom';
 
-function BookingCard({ booking, userRole, onEdit, onAction }) {
-  
+function BookingCard({ booking, userRole, onEdit, onAction,onPaymentUpdate }) {
+  const navigate = useNavigate();
   const getStatusColor = (status) => {
     if (status === 'Approved') return 'bg-green-100 text-green-800';
     if (status === 'Rejected') return 'bg-red-100 text-red-800';
     return 'bg-yellow-100 text-yellow-800'; // Pending
   };
+  const container = booking.container
   const isAdmin = userRole === 'admin' || userRole === 'superuser';
 
-  const canEdit = userRole === 'sales' && (booking.status === 'Pending' || booking.status === 'Approved');
+  let etaDate;
+  if (container.eta && container.eta !== 'N/A') {
+    etaDate = format(container.eta, 'dd-MMM');
+  } else {
+    etaDate = 'N/A';
+  }
+
+  const bookingDetails = `₹${booking.pricePerPiece}/psc, ${booking.battery?.included ? 'With Battey, ' : ''} ${booking.charger?.included ? 'With Charger, ' : ''} ${booking.tyre?.included ? 'With tyre, ' : ''} ${booking.assembling?.included ? 'With Assembling, ' : ''} and ${booking.transport?.included ? 'With Transport' : `₹${booking.transport?.charges} Transport extra`}`
 
   return (
-    <div className="bg-white p-4 rounded-sm shadow-sm border border-gray-200 w-full">
-        <div className="flex justify-between items-start">
-          <h3 className="font-bold text-lg text-blue-700">{booking.container?.container_no}</h3>
-          <div className="flex items-center space-x-2 gap-2 w-auto">
-            {(userRole === 'superuser') && booking.status === 'Pending' && (
-            <>
-                <Button onClick={() => onAction('reject', booking)} className="hover:bg-red-700 text-gray-500 flex justify-center rounded-sm !w-fit !p-2" bgColor="bg-red-600">
-                    <XCircle size={18}/>
-                </Button>
-                <Button onClick={() => onAction('approve', booking)} className="hover:bg-green-700 text-gray-500 flex justify-center rounded-sm !w-fit !p-2" bgColor="bg-green-600">
-                    <CheckCircle size={18} />
-                </Button>
-            </>
-            )}
-            <Button type="submit" className="hover:bg-blue-600 text-gray-500 flex justify-center rounded-sm !w-fit !p-2" onClick={() => onEdit({...booking})}>
-                <SquarePen size={18}/>
-            </Button>
-            <Button type="submit" className="hover:bg-stone-600 text-gray-500 flex justify-center rounded-sm !w-fit !p-2" bgColor="bg-stone-600" onClick={() => onAction('delete', booking)}>
-                <Trash size={18} />
-            </Button>
-            <span className={`px-3 py-1 text-xs font-semibold rounded-full ${getStatusColor(booking.status)}`}>
-                {booking.status}
-            </span>
-      </div>
-        </div>
-        <div className='mt-2 '>
-          <h2 className='text-base font-medium text-gray-600 mb-1'>Container Details:</h2>
-          <div className='flex flex-col md:flex-row gap-1.5 flex-wrap md:gap-x-6 md:gap-y-2'>
-              {isAdmin && 
-                        <>
-                            <p><strong className="text-gray-600">Company:</strong> {booking.container?.company_name}</p>
-                            <p><strong className="text-gray-600">Status:</strong> {booking.container?.status}</p>
-                        </>
-                }
-              <p className="text-sm text-gray-600"><strong>Model:</strong> {booking.container?.model}</p>
-              <p className="text-sm text-gray-600"><strong>Specifications:</strong> {booking.container?.specifications}</p>
-              <p className="text-sm text-gray-600"><strong>Quantity:</strong> ₹{booking.container?.qty}</p>
-              <p className="text-sm text-gray-600"><strong>Port:</strong>{booking.container?.port}</p>
-          </div>
-        </div>
-        
-        <div className='mt-2 '>
-          <h2 className='text-base font-medium text-gray-600 mb-1'>Booking Details:</h2>
-          <div className='flex flex-col md:flex-row gap-1.5 flex-wrap md:gap-x-6 md:gap-y-2'>
-              <p className="text-sm text-gray-600"><strong>Dealer:</strong> {booking.dealer?.trade_name}</p>
-              <p className="text-sm text-gray-600"><strong>Place of Delivery:</strong> {booking.placeOfDelivery}</p>
-              <p className="text-sm text-gray-600"><strong>Price:</strong> ₹{booking.pricePerPiece}/psc</p>
-              <p className="text-sm text-gray-600"><strong>Freight:</strong> {booking.transport.included === 'true' ? 'Included' : `₹${booking.transport.charges} Extra`}</p>
-              <p className="text-sm text-gray-600">
-                  {booking.withBattery || booking.withCharger || booking.withTyre ? (<strong>Extras:</strong>
-                  ) : (
-                  <strong>No Extras</strong>
-                  )} 
-                  {' '}
-                  {booking.withBattery ? 'With Battery' : ''} {booking.withCharger ? 'With Charger' : ''} {booking.withTyre ? 'With Tyre' : ''} 
-              </p>
-              {/* <p className="text-sm text-gray-600"> 
-              <strong>Extras:</strong> {booking.withBattery ? 'With Battery' : ''} {booking.withCharger ? 'With Charger' : ''} {booking.withTyre ? 'With Tyre' : ''}  </p> */}
-              <p className="text-sm text-gray-500 w-full">
-              Remarks: {booking.remarks || 'N/A'}
-              </p>
-          </div>
-        </div>
-
-
-        {booking.status === 'Rejected' && (
-            <div className="mt-2 p-2 md:px-6 bg-red-50 border-l-4 border-red-400 w-fit">
-            <p className="text-xs text-red-700"><strong>Reason:</strong> {booking.rejectionReason || 'No reason provided.'}</p>
-            <Button className="mt-2 text-xs">Resend Request</Button>
+    <>
+      <div
+        className={`bg-white p-4 md:py-6 flex hover:bg-slate-50 flex-col cursor-pointer justify-start border border-gray-200 lg:border-0 lg:border-b-1 lg:grid ${isAdmin ? 'lg:grid-cols-13' : 'lg:grid-cols-11'} lg:gap-3 lg:items-center lg:text-center`}>
+        {/* Container No (1 cols) */}
+        <div
+          className={`mb-4 lg:mb-0 text-md flex flex-col gap-3/2 pb-2 lg:pb-0 border-b-1 lg:border-b-0 border-gray-200 ${isAdmin ? 'lg:col-span-2' : 'lg:col-span-1'}`}
+          onClick={() => navigate(`/bookings/${booking.id}`)}
+        >
+          {isAdmin && (
+            <div className="flex items-center gap-1 lg:justify-center mb-1">
+              <div
+                className={`py-1/2 px-2 flex lg:flex items-center gap-1 text-[10px] leading-5 font-bold rounded-full outline ${
+                  containerStatusMap.find((doc) => (doc.status === container.status ? true : null))
+                    ?.colour
+                }`}
+              >
+                <div className="h-2 w-2 rounded-full bg-white outline"></div>
+                {container.status.toUpperCase()}
+              </div>
             </div>
-        )}
-        {isAdmin && (
-          <p className="text-xs text-gray-400 mt-3 pt-2 border-t">
-            Requested By: {booking.requested_by_name}
-          </p>
-        )}
-      {/* <div className="mt-4 pt-2 border-t flex justify-end items-center space-x-2">
-        {canEdit && (
-          <Button onClick={() => onEdit(booking)} size="sm" variant="outline">
-            <Edit2 size={14} className="mr-1" /> Edit
-          </Button>
-        )}
-        
-      </div> */}
-    </div>
+          )}
+          <p className="font-semibold text-blue-700">{container.container_no}</p>
+        </div>
+          {/* Sales Person */}
+        {isAdmin && (<div className="mb-2 lg:mb-0 flex lg:flex-col text-sm gap-1 lg:gap-0  flex-wrap" onClick={() => navigate(`/bookings/${booking.id}`)}>
+          <p className="font-normal lg:hidden lg:text-xs text-gray-500">Sales Person:</p>
+          <p className="text-sm">{booking.requested_by_name}</p>
+        </div>)}
+        {/* Model & Specs (2 cols) */}
+        <div className="lg:col-span-2 mb-2 lg:mb-0 flex lg:flex-col text-sm gap-1 lg:gap-0  flex-wrap" onClick={() => navigate(`/bookings/${booking.id}`)}>
+          <p className="font-normal lg:hidden lg:text-xs text-gray-500">Model & Specs:</p>
+          <p className="lg:font-normal">{container.model}</p>
+          <p className="lg:text-xs lg:text-gray-500">{container.specifications}</p>
+        </div>
+
+
+        {/* Qty (2 col) */}
+        <div className="lg:col-span-2 mb-2 lg:mb-0 flex lg:flex-col text-sm gap-1 lg:gap-0  flex-wrap" onClick={() => navigate(`/bookings/${booking.id}`)}>
+          <p className="font-normal lg:hidden lg:text-xs text-gray-500">Dealer:</p>
+          <p className="text-sm">{booking.dealer?.trade_name}</p>
+          <p className="text-sm">{booking.placeOfDelivery}</p>
+        </div>
+
+        {/* Booking Details (2 col) */}
+        <div className="lg:col-span-2 mb-2 lg:mb-0 flex lg:flex-col text-sm gap-1 lg:gap-0  flex-wrap" onClick={() => navigate(`/bookings/${booking.id}`)}>
+          <p className="font-normal lg:hidden lg:text-xs text-gray-500">Booking Details:</p>
+          <p className="text-sm">{bookingDetails}</p>
+        </div>
+        {/* Payment (1 col) */}
+        <div className="mb-2 lg:mb-0 flex lg:flex-col text-sm gap-1 lg:gap-0 flex-wrap" onClick={() => navigate(`/bookings/${booking.id}`)}>
+          <p className="font-normal lg:hidden lg:text-xs text-gray-500">Payment Status:</p>
+          <div className="flex flex-col gap-1">
+            <span className={`px-3 py-1 text-xs font-semibold rounded-full ${
+              getPaymentStatusColor(calculatePaymentStatus(booking.payment?.amountPaid || 0, booking.grandTotal))
+            }`}>
+              {calculatePaymentStatus(booking.payment?.amountPaid || 0, booking.totals?.grandTotal)}
+            </span>
+          </div>
+        </div>
+        {/* Status (1 col) */}
+        <div className="mb-2 lg:mb-0 flex lg:flex-col text-sm gap-1 lg:gap-0 flex-wrap" onClick={() => navigate(`/bookings/${booking.id}`)}>
+          <p className="font-normal lg:hidden lg:text-xs text-gray-500">Booking Status:</p>
+          <p className="text-sm"><span className={`px-3 py-1 text-xs font-semibold rounded-full ${getStatusColor(booking.status)}`}>
+              {booking.status}</span></p>
+        </div>
+
+        {/* Actions (2 col on desktop) */}
+        <div className="lg:col-span-2 flex flex-row lg:justify-around justify-between lg:pt-0 lg:items-center gap-2 lg:mt-0 pt-4">
+          {userRole === 'superuser' && booking.status === 'Pending' && (
+              <>
+                <Button
+                  onClick={() => onAction('reject', booking)}
+                  variant='ghost'
+                  size='small'
+                  textColor="text-red-700"
+                >
+                  <XCircle size={18} />
+                </Button>
+                <Button
+                  onClick={() => onAction('approve', booking)}
+                  variant='ghost'
+                  size='small'
+                  textColor="text-green-700"
+                >
+                  <CheckCircle size={18} />
+                </Button>
+              </>
+            )}
+            <Button
+              type="submit"
+              variant='ghost'
+              size='small'
+              textColor='text-gray-500'
+              onClick={() => onEdit({...container} ,{ ...booking })}
+            >
+              <SquarePen size={18} />
+            </Button>
+            <Button
+              type="submit"
+              variant='ghost'
+              size='small'
+              textColor='text-blue-500'
+              onClick={() => onPaymentUpdate({ ...booking })}
+            >
+              <Banknote size={18} />
+            </Button>
+            <Button
+              type="submit"
+              variant='ghost'
+              size='small'
+              textColor='text-red-500'
+              onClick={() => onAction('delete', booking)}
+            >
+              <Trash size={18} />
+            </Button>
+        </div>
+      </div>
+    </>
   );
 }
 
--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,28 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo  } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { selectUser } from '../features/user/userSlice';
 import { selectAllContainers, selectContainerStatus, fetchContainers, setContainersStatus } from "../features/containers/containersSlice";
 import { selectAllDealers, selectDealersStatus, fetchDealers } from "../features/dealers/dealersSlice";
 import { selectAllBookings, selectBookingsStatus, fetchBookings, setBookingsStatus } from "../features/bookings/bookingsSlice";
-// import { selectAllUsers, selectUsersStatus, fetchUsers } from "../slices/userSlice";
 import bookingService from '../firebase/bookings';
-import {BookingForm as BookingFormModal, BookingCard, Container, StatCard,ConfirmationAlert,Loading, Tabs, SearchBar} from '../components';
-import { AlertTriangle, CheckCircle, XCircle } from 'lucide-react';
+import {BookingCard, Container, StatCard,Loading, Tabs, SearchBar, Button, PaymentUpdateModal} from '../components';
+import { AlertCircle, AlertTriangle, CheckCircle, FileText, PlusCircle, XCircle } from 'lucide-react';
 import { Ship, Anchor, ListChecks } from 'lucide-react';
 import {bookingTabs , salesColumns } from '../assets/utils';
-
+import { useModal } from '../contexts/ModalContext';
+import { useBooking } from '../contexts/BookingContext';
+import { useNavigate } from 'react-router-dom';
 
 function BookingsPage() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [bookings, setBookings] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [bookingToEdit, setBookingToEdit] = useState(null);
   const userData = useSelector(selectUser);
   const [isUpdating, setIsUpdating] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
   const [activeTab, setActiveTab] = useState('ALL');
   const [searchQuery, setSearchQuery] = useState('');
   const isAdmin = ['admin', 'superuser'].includes(userData?.role);
+  const [paymentModal, setPaymentModal] = useState({
+    isOpen: false,
+    selectedBooking: null
+  });
 
   // Redux state
   const containers = useSelector(selectAllContainers);
@@ -38,6 +43,8 @@ function BookingsPage() {
     // requested_by_name: userData.displayName || 'Unknown',
   }));
   
+  const { openBookingModal } = useBooking();
+  const { showModal } = useModal();
 
   const containersStatus = useSelector(selectContainerStatus);
   const dealersStatus = useSelector(selectDealersStatus);
@@ -63,21 +70,89 @@ function BookingsPage() {
   }
   }, [dispatch, containersStatus, dealersStatus, bookingsStatus, userData]);
 
+  // const TabsOptions =  [...new Set(bookings?.map(c => c.generated_by_name?.trim().toUpperCase()).filter(Boolean))].map(u => { return {name: u}})
+  const processedBookings = useMemo(() => {
+        let processed = [...bookings]
+        bookingTabs.forEach(tab => {
+          if(activeTab === 'APPROVAL PENDING') {
+            processed = processed.filter(b => b.status.toLowerCase() === 'pending');
+          }
+          else if(activeTab === 'APPROVED AND ACTIVE') {
+            processed = processed.filter(b => b.status.toLowerCase() === 'approved' && b.container?.status !== 'Reached Destination');
+          } else if (activeTab === 'COMPLETED') {
+            processed = processed.filter(b => b.status.toLowerCase() === 'approved' && b.container?.status === 'Reached Destination');
+          }
+        })
+        if (searchQuery) {
+          const lowercasedQuery = searchQuery.toLowerCase();
+          processed = processed.filter(b => 
+            String(b.containerId).toLowerCase().includes(lowercasedQuery) ||
+            String(b.dealer.trade_name).toLowerCase().includes(lowercasedQuery) ||
+            String(b.placeOfDelivery).toLowerCase().includes(lowercasedQuery) ||
+            String(b.requested_by_name).toLowerCase().includes(lowercasedQuery) ||
+            String(b.status).toLowerCase().includes(lowercasedQuery)
+          );
+        }
+        return processed;
+      }, [bookings, searchQuery, activeTab]);
 
-
-  const openConfirmationModal = (action, booking) => {
-    setAlertState({ isOpen: true, action, booking });
+  const handleUpdatePayment = (booking) => {
+    setPaymentModal({
+      isOpen: true,
+      selectedBooking: booking
+    });
   };
-
-  const closeConfirmationModal = () => {
-    setAlertState({ isOpen: false, action: null, booking: null });
+  const handlePaymentUpdate = async (paymentData) => {
+    try {
+      const { container, dealer, ...bookingData } = paymentModal.selectedBooking;
+      const finalData = {...bookingData, 
+        payment: {
+          ...paymentModal.selectedBooking.payment,
+          amountPaid: paymentData.amountPaid,
+          notes: paymentData.notes,
+          status: paymentData.status,
+          transactions: [
+            ...(paymentModal.selectedBooking.payment?.transactions || []),
+            {
+              amount: paymentData.amountPaid - (paymentModal.selectedBooking.payment?.amountPaid || 0),
+              date: paymentData.transactionDate,
+              id: paymentData.transactionId,
+            }
+          ]
+        }
+      }
+      // Update booking with new payment information
+      await bookingService.updateBooking(paymentModal.selectedBooking.id, finalData);
+      // Refresh bookings data
+      dispatch(fetchBookings({ role: userData.role, userId: userData.uid }));
+      setPaymentModal({ isOpen: false, selectedBooking: null });
+    } catch (error) {
+      console.error('Failed to update payment:', error);
+    }
   };
+  
+  const handleEdit = (container, booking) => {
+    openBookingModal(container, booking);
+  };
+  
+  
 
+  useEffect(() => {
+      if (alertState.isOpen && alertState.action) {
+        const modalConfig = getModalConfig();
+        if (modalConfig) {
+          showModal({
+            ...modalConfig,
+            onConfirm: handleConfirmAction,
+          });
+        }
+      }
+    }, [alertState]); 
+  
   const handleConfirmAction = async (reason = '') => {
     const { action, booking } = alertState;
     if (!action || !booking) return;
     setIsUpdating(true);
-    closeConfirmationModal();
     try {
       switch (action) {
         case 'approve':
@@ -105,24 +180,6 @@ function BookingsPage() {
     }
   };
 
-  const handleEdit = (booking) => {
-    setBookingToEdit(booking);
-    setIsModalOpen(true);
-  };
-
-  
-  const handleFormSubmit = async (formData) => {
-    if (bookingToEdit) {
-      const { container, dealer, ...bookingData } = formData;
-      await bookingService.updateBooking(bookingToEdit.id, bookingData);
-      // Refresh the list to show the updated "Pending" status
-      setBookings(completeBookingData);
-      // bookingService.getBookings(userData.uid, userData.role).then(setBookings);
-    }
-    setIsModalOpen(false);
-    setBookingToEdit(null);
-  };
-
   const getModalConfig = () => {
     switch (alertState.action) {
       case 'approve':
@@ -136,30 +193,26 @@ function BookingsPage() {
     }
   };
 
-  const pendingBookings = bookings.filter(b => b.status === 'Pending').length;
-  const activeBookings = bookings.filter(b => b.status === 'Approved' && b.container.status !== 'Reached Destination').length;
+  const pendingBookings = processedBookings.filter(b => b.status === 'Pending').length;
+  const activeBookings = processedBookings.filter(b => b.status === 'Approved' && b.container.status !== 'Reached Destination').length;
+
+  if (loading) {
+    return <Loading isOpen={true} message="Loading Booking Page..." />
+  }
 
   return (
    <Container>
      <div className="w-full bg-white rounded-2xl p-4 sm:p-6 lg:p-8 shadow-md">
        <div className="w-full flex flex-col justify-between mb-10">
         <h1 className="text-3xl font-bold text-gray-800">Booking Requests</h1>
-        {/* <div className="mt-6 flex justify-end">
-            <Button
-                onClick={() => handleOpenForm()}
-                className="bg-blue-600 hover:bg-blue-700 !rounded-4xl transition md:w-auto flex items-center"
-                >
-                <PlusCircle size={20} className="mr-2"/> Register Dealer
-            </Button>
-        </div> */}
       </div>
       <Loading isOpen={isUpdating} message="Syncing with server..." />
       {loading && <Loading isOpen={isUpdating} message="Loading the bookings..." />}
       {!loading && bookings.length === 0 && (
-        <p className="text-gray-600">No bookings found. Go to dashboard and submit new booking requests</p>
+        <p className="text-gray-600"></p>
       )}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-              <StatCard title="Total Bookings" value={bookings.length} icon={<ListChecks className="text-blue-500" />} />
+              <StatCard title="Total Bookings" value={processedBookings.length} icon={<ListChecks className="text-blue-500" />} />
               <StatCard title="Pending Bookings" value={pendingBookings} icon={<Ship className="text-teal-500" />} />
               <StatCard title="Active Bookings" value={activeBookings} icon={<Anchor className="text-indigo-500" />} />
         </div> 
@@ -167,47 +220,61 @@ function BookingsPage() {
             {isAdmin && <Tabs tabs={bookingTabs} activeTab={activeTab} onTabClick={setActiveTab} />}
             <div className="flex flex-col lg:flex-row justify-between items-center py-6 gap-4">
             <SearchBar
-            placeholder= {'Search by Firm name, Gst no, District, State...'}
-            query={searchQuery} setQuery={setSearchQuery} className='rounded-xs py-2' resultCount={bookings.length}/>
+            placeholder= {'Search by container no, dealer, place of deliver, status...'}
+            query={searchQuery} setQuery={setSearchQuery} className='rounded-xs py-2' resultCount={processedBookings.length}/>
             </div>
       </div>
-       <div className={`hidden lg:grid ${isAdmin ? 'grid-cols-11' : 'grid-cols-10'}  gap-4 px-4 py-2 font-normal text-md text-gray-600 text-center bg-gray-50 rounded-lg`}>
-          {isAdmin ? <div className='col-span-2'>Container & Company</div> : 
-            <div>Container</div>}
-            <div className="col-span-2">Model & Specs</div>
-            <div>Qty</div>
-            <div className="col-span-2">Colours</div>
-            <div className="col-span-2">Battery & Chargers</div>
-            <div>ETA / Port</div>
-            <div>Actions</div>
+        {error && 
+          <div className='flex flex-col items-center py-12 text-center'>
+          <div className="w-12 h-12 bg-red-100 rounded-full flex items-center justify-center mb-4">
+            <AlertCircle className='w-8 h-8 text-red-600'/>
+          </div>
+          <h3 className='text-lg font-medium text-slate-900 mb-2'>Error Occured</h3>
+          <p className="text-red-500  mb-6 max-w-md">Error: {error}</p>
+        </div>      
+        }
+        {!error && (processedBookings.length === 0 ? <div className='flex flex-col items-center py-12 text-center'>
+        <div className="w-16 h-16 bg-slate-100 rounded-full flex items-center justify-center mb-4">
+          <FileText className='w-8 h-8 text-slate-400'/>
         </div>
-      {!loading && (
-        <div className="space-y-4">
-          {bookings.map(booking => (
-            <BookingCard
-              key={booking.id}
-              booking={booking}
-              userRole={userData.role}
-              onEdit={handleEdit}
-              onAction={openConfirmationModal}
-            />
-          ))}
-        </div>
-      )}
-    <BookingFormModal
-        bookingToEdit={bookingToEdit} 
-        container={bookingToEdit?.container}
-        onSubmit={handleFormSubmit}
-        isOpen={isModalOpen}
-        onCancel={() => setIsModalOpen(false)}
-    />
-    <ConfirmationAlert
-      isOpen={alertState.isOpen}
-      onClose={closeConfirmationModal}
-      onConfirm={handleConfirmAction}
-      {...getModalConfig()}
-    />
+        <h3 className='text-lg font-medium text-slate-900 mb-2'>No bookings found</h3>
+        <p className="text-slate-500 mb-6 max-w-md">{bookings.length === 0 ? 'You have not registered any booking yet.' : 'Your search or filter criteria did not match any booking. Try adjusting search.'}</p>
+        {bookings.length === 0 && (
+          <Button onClick={() => navigate('/sales')} className=" hover:bg-blue-700 !rounded-4xl transition md:w-auto flex items-center">
+            <PlusCircle size={20} className="mr-2"/>Create First Booking</Button>
+        )}
+      </div> : <>
+          <div className={`hidden lg:grid ${isAdmin ? 'grid-cols-13' : 'grid-cols-11'}  gap-4 px-4 py-2 items-center font-normal text-md text-gray-600 text-center bg-gray-50 rounded-lg`}>
+            {isAdmin ? <div className='col-span-2' >Container #</div> : 
+              <div>Container #</div>}
+              {isAdmin && <div>Sales Person</div>}
+              <div className="col-span-2">Model & Specs</div>
+              <div className="col-span-2">Dealer</div>
+              <div className="col-span-2">Booking Details</div>
+              <div>Payment Status</div>
+              <div>Booking Status</div>
+              <div className="col-span-2">Actions</div>
+          </div>
+          <div className="">
+            {processedBookings.map(booking => (
+              <BookingCard
+                key={booking.id}
+                booking={booking}
+                userRole={userData.role}
+                onEdit={handleEdit}
+                onPaymentUpdate={handleUpdatePayment}
+                onAction={(action, booking)=> setAlertState({ isOpen: true, action, booking })}
+              />
+            ))}
+          </div>
+      </>)}
      </div>
+     <PaymentUpdateModal
+        booking={paymentModal.selectedBooking}
+        isOpen={paymentModal.isOpen}
+        onClose={() => setPaymentModal({ isOpen: false, selectedBooking: null })}
+        onUpdate={handlePaymentUpdate}
+      />
    </Container>
   );
 }
