055f00f1d388fdd3f8e8b8869a5aae9f69a5f691
Shaurya <j.shaurya@iitg.ac.in>
2025-10-21 23:53:23 +0530

    changed the loader style

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,13 @@
 import { Loader2 } from 'lucide-react'; 
 import {ModalContainer} from './index'
+import './stylesheets/loader.css'
 
 function Loading({ isOpen, message = "Processing..." }) {
   if (!isOpen) return null;
 
   return (
     <ModalContainer isOpen={isOpen} title="loading" className='max-w-fit flex gap-4'>
-        <Loader2 className="animate-spin h-8 w-8 text-blue-600" />
+        <div className="container"><div className="line"></div></div>
         <p className="text-lg font-medium text-gray-700">{message}</p>
     </ModalContainer>
     
