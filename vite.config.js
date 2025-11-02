e17d8b69377491c6a70da4f2e9c179bf248cfce2
Shaurya <j.shaurya@iitg.ac.in>
2025-11-02 21:18:35 +0530

    Refactor ESLint and configuration files for consistency and style improvements

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { analyzer } from 'vite-bundle-analyzer'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { analyzer } from 'vite-bundle-analyzer';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss({
-    config: {
+  plugins: [
+    react(),
+    tailwindcss({
+      config: {
         // You can put your full tailwind config here
-        content: [
-          "./index.html",
-          "./src/**/*.{js,ts,jsx,tsx}",
-        ],
+        content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
         theme: {
           extend: {},
         },
@@ -19,11 +18,13 @@ export default defineConfig({
         future: {
           hoverOnlyWhenSupported: true,
         },
-        plugins: []},}),
-      analyzer(),
-      ],
-  server: {
-        port: 3000, // Unique port for the first app
-        host: 'localhost', // Or '127.0.0.1'
+        plugins: [],
       },
-})
+    }),
+    analyzer(),
+  ],
+  server: {
+    port: 3000, // Unique port for the first app
+    host: 'localhost', // Or '127.0.0.1'
+  },
+});
